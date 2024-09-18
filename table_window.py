a649452218b5d20f5f476c062e0bc38e5abc3d61
Nikola Lakic <nlakic@pm.me>
2024-09-18 19:53:31 +0200

    Dodate default vrednosti modalnih masa
    Iz razloga brze analize

--- table_window.py
+++ table_window.py
@@ -1,6 +1,5 @@
 from PySide6 import QtWidgets
 
-
 class TableWindow(QtWidgets.QWidget):
     _instance = None
 
@@ -23,12 +22,17 @@ class TableWindow(QtWidgets.QWidget):
             self.table_widget.setColumnCount(1)
             self.table_widget.setHorizontalHeaderLabels(['Modal mass [kg]'])
             self.setGeometry(610, 375, 150, 300)
-
+            self.default_values = [938.97, 398.08, 245.01, 320.93]
             self.modal_masses = [0.0] * self.svs_num
 
             for row in range(self.svs_num):
-                item = QtWidgets.QTableWidgetItem("0.0")
+                if self.svs_num == 4:
+                    item_value = self.default_values[row]
+                else:
+                    item_value = 0.0
+                item = QtWidgets.QTableWidgetItem(f"{item_value:.2f}")
                 self.table_widget.setItem(row, 0, item)
+                self.modal_masses[row] = item_value
 
             self.table_widget.itemChanged.connect(self.update_modal_masses)
             layout.addWidget(self.table_widget)
@@ -45,21 +49,17 @@ class TableWindow(QtWidgets.QWidget):
     def update_modal_masses(self, item):
         row = item.row()
         value = item.text().strip()
-        # print(f"Item changed in row {row}: '{value}'")
         try:
             float_value = float(value)
             self.modal_masses[row] = float_value
-            #print(f"Updated modal mass at row {row}: {self.modal_masses[row]}")
         except ValueError:
             self.modal_masses[row] = 0.0
             QtWidgets.QMessageBox.warning(self, "Invalid Input", "Please enter a valid number.")
-        # print("Current modal_masses list:", self.modal_masses)  # Debug print
 
     def save_and_exit(self):
         self.close()
 
     def closeEvent(self, event):
-        # print("Modal masses before exiting:", self.modal_masses)
         event.accept()
 
     def get_modal_masses(self):
